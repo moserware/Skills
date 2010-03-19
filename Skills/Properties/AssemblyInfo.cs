e01f7d16209f0e4dd126ede6eb60694fd1b9d594
Daniel Lee Harple <dlh+dev@generalconsumption.org>
2010-03-19 05:04:40 -0400

    Always make internals visible to UnitTests, so tests can be run in non-debug builds.

--- Skills/Properties/AssemblyInfo.cs
+++ Skills/Properties/AssemblyInfo.cs
@@ -38,8 +38,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-#if DEBUG
-
 [assembly: InternalsVisibleTo("UnitTests")]
-#endif
